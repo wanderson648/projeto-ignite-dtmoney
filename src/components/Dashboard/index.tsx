f06b2a48b1ff23c2ba77cf28bb36786056afc847
Wanderson Oliveira <testepedroca@gmail.com>
2021-03-26 23:19:07 -0300

    Componente: TransactionsTable

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../Summary";
+import { TrasactionTable } from "../TrasactionsTable";
 import { Container } from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
       <Summary />
+      <TrasactionTable />
     </Container>
   );
 }
