d16349822f62875c3c41af317adefb1c020532d7
Wanderson Oliveira <testepedroca@gmail.com>
2021-04-09 19:15:30 -0300

    Estilizando modal

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Modal from 'react-modal';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal/NewTransactionModal';
 import { GlobalStyle } from './styles/global';
 
 Modal.setAppElement('#root');
@@ -18,18 +19,15 @@ export function App() {
     setIsNewTransactionModalOpen(false);
   }
 
-
   return (
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
 
-      <Modal
-          isOpen={isNewTransactionModalOpen} 
-          onRequestClose={handleCloseNewTransactionModal}
-        >
-        <h2>Cadastrar Transação</h2>
-      </Modal>
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
 
       <GlobalStyle/>
     </>
