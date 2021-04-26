c949f8852387623f36b1f5aaf69026ac738d2088
Wanderson Oliveira <testepedroca@gmail.com>
2021-04-26 17:07:45 -0300

    Funcionamento dos botões

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import Modal from 'react-modal';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
-import { NewTransactionModal } from './components/NewTransactionModal/NewTransactionModal';
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from './styles/global';
 
 Modal.setAppElement('#root');
