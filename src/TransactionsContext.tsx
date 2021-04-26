bd1cb8bb5511598d3d955738d8228953bffeea07
Wanderson Oliveira <testepedroca@gmail.com>
2021-04-26 19:37:34 -0300

    Finalizando inserção

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -18,7 +18,7 @@ interface TransactionsProviderProps {
 
 interface TransactionsContextData {
   transactions: Transaction[];
-  createTransaction: (transaction: TransactionInput) => void;
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
 export const TransactionsContext = createContext<TransactionsContextData>(
@@ -35,8 +35,14 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
   }, []);
 
 
-  function createTransaction(transaction: TransactionInput) {
-    api.post('/transactions', transaction);
+  async function createTransaction(transactionInput: TransactionInput) {
+    const response = await api.post('/transactions', {
+      ...transactionInput,
+      createdAt: new Date(),
+    });
+    const { transaction } = response.data;
+
+    setTransactions([ ...transactions, transaction ]);
   }
 
   return (
--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -25,15 +25,20 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
 
   const [type, setType] = useState('deposit');
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    createTransaction({
+    await createTransaction({
       title,
       amount,
       category,
       type
     })
+
+    setTitle('');
+    setAmount(0)
+    setCategory('')
+    onRequestClose();
   }
 
    return (
